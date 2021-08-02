93d9bbb60b7ae8ba6a5d4c519c2f224ebb74f67e
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-02 14:28:35 +0300

    Add files via upload

---
+++ donguler-for-loop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tekToplam = 0;
+            int ciftToplam = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 1) tekToplam += i;
+                else ciftToplam += i;
+            }
+            Console.WriteLine(tekToplam);
+            Console.WriteLine(ciftToplam);
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
