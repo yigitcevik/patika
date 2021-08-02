cf0a1f87bfa2baea16dde0c68ac4bd7aa426cfb8
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-02 14:59:05 +0300

    Add files via upload

---
+++ donguler-while-foreach.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı gir: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac ++;
+            }
+            Console.WriteLine(toplam/sayi);    
+
+            char character = 'a';   
+            while (character <= 'z')
+            {
+                Console.Write(character);
+                character ++;
+            }     
+
+            string[] arabalar = {"Mercedes", "BMW", "Toyota", "Ford"};
+            foreach (var item in arabalar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
