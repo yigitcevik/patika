24ff7e8a1f35acad32053ac8ca4c62e40dcafa1a
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-02 12:57:47 +0300

    Add files via upload

---
+++ hata-yonetimi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sayı: " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.Write("İşlem tamamlandı");
+            }
+
+            try
+            {
+                int a = int.Parse("test");
+                int b = int.Parse("-21345678903");
+            }
+            catch (ArgumentNullException ex)
+            {   
+                Console.WriteLine("Boş Değer");
+                Console.WriteLine(ex.Message.ToString());
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi yanlış");
+                Console.WriteLine(ex.Message.ToString());
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada çok büyük değer");
+                Console.WriteLine(ex.Message.ToString());
+            }
+            finally
+            {
+                Console.Write("İşlem tamamlandı");
+            }
+
+
+        }
+    }
+}
