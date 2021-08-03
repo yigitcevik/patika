fc092f69c97c67a9273e7c62db0299fad1774aa7
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-03 17:46:41 +0300

    Add files via upload

---
+++ metotlar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(Topla(a, b)));
+            ornek.EkranaYazdir(Convert.ToString(ornek.ArttirTopla(ref a,ref b)));
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+
+        }
+
+        static int Topla(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirTopla(ref int a,ref int b)
+        {
+            a++;
+            b++;
+            return a + b;
+        }
+    }
+}
