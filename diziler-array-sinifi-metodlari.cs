23f1286798e831807e8c84698068e5be3b538337
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-03 13:11:09 +0300

    Add files via upload

---
+++ diziler-array-sinifi-metodlari.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace diziler_array_sinifi_metodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************** Sırasız *************");
+            int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+            foreach (var sayi in sayiDizisi) Console.WriteLine(sayi);
+
+            Console.WriteLine("************** Sıralı *************");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi) Console.WriteLine(sayi);
+
+            Console.WriteLine("************** Clear *************");
+            Array.Clear(sayiDizisi, 2, 2);
+            foreach (var sayi in sayiDizisi) Console.WriteLine(sayi);
+
+            Console.WriteLine("************** Reverse *************");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi) Console.WriteLine(sayi);
+
+            Console.WriteLine("************** IndexOf *************");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+            Console.WriteLine("************** Resize *************");
+            Array.Resize<int>(ref sayiDizisi, 9);
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi) Console.WriteLine(sayi);
+        }
+    }
+}
