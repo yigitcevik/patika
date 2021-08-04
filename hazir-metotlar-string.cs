db5b3fcf70941b6f2fa104f6a57bd180a5f866f6
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-04 21:27:59 +0300

    Add files via upload

---
+++ hazir-metotlar-string.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace hazir_metotlar_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken = "Dersimiz CSharp, Hoşgeldiniz!";
+            string degisken2 = "CSharp";
+
+            Console.WriteLine(degisken.Length);
+
+            Console.WriteLine(degisken.ToUpper());
+            Console.WriteLine(degisken.ToLower());
+
+            Console.WriteLine(String.Concat(degisken, " Merhaba!"));
+
+            Console.WriteLine(degisken.CompareTo(degisken2));
+            Console.WriteLine(String.Compare(degisken, degisken2, true));
+            Console.WriteLine(String.Compare(degisken, degisken2, false));
+
+            Console.WriteLine(degisken.Contains(degisken2));
+            Console.WriteLine(degisken.EndsWith("Hoşgeldiniz!"));
+            Console.WriteLine(degisken.StartsWith("Merhaba"));
+
+            Console.WriteLine(degisken.IndexOf("CSharp"));
+            Console.WriteLine(degisken.LastIndexOf("i"));
+
+            Console.WriteLine(degisken.Insert(0,"Merhaba "));
+            
+            Console.WriteLine(degisken + degisken2.PadLeft(30, '*'));
+
+            Console.WriteLine(degisken.Remove(10));
+            Console.WriteLine(degisken.Remove(5,3));
+            
+            Console.WriteLine(degisken.Replace("CSharp", "C#"));
+            Console.WriteLine(degisken.Replace(" ", "*"));
+
+            Console.WriteLine(degisken.Split(" ")[1]);
+
+            Console.WriteLine(degisken.Substring(4));
+            Console.WriteLine(degisken.Substring(4,6));
+        }
+    }
+}
