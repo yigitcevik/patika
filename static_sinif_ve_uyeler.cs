d48db9e42b59edfb57d52d589841d9d90ac95531
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-06 18:30:00 +0300

    Add files via upload

---
+++ static_sinif_ve_uyeler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace static_sinif_ve_uyeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çalışan sayısı: {0}", Calisan.CalisanSayisi);
+            Calisan calisan = new Calisan("Ayşe", "Yılmaz", "İK");
+            Console.WriteLine("Çalışan sayısı: {0}", Calisan.CalisanSayisi);
+            Calisan calisan1 = new Calisan("Deniz", "Ada", "İK");
+            Console.WriteLine("Çalışan sayısı: {0}", Calisan.CalisanSayisi);
+
+            
+            Console.WriteLine("Toplam: {0}", Islemler.Topla(100,200));
+            Console.WriteLine("Çıkarma: {0}", Islemler.Cikar(500,200));
+        }
+    }
+
+    class Calisan
+    {
+        private static int calisanSayisi;
+
+        public static int CalisanSayisi { get => calisanSayisi; }
+
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+
+        static Calisan()
+        {
+            calisanSayisi = 0;
+        }
+
+        public Calisan(string ısim, string soyisim, string departman)
+        {
+            this.Isim = ısim;
+            this.Soyisim = soyisim;
+            this.Departman = departman;
+            calisanSayisi++;
+        }
+    }
+
+    static class Islemler
+    {
+        public static long Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public static long Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+}
