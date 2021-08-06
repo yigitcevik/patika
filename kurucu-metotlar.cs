31c419add2aa172aeff2102a42af1b49d965a68a
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-06 13:59:31 +0300

    Add files via upload

---
+++ kurucu-metotlar.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace kurucu_metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan("Ayşe", "Kara", 23415634, "İnsan Kaynakları");
+            calisan1.CalisanBilgileri();
+
+            Calisan calisan2 = new Calisan("Deniz", "Arda", 15646789, "Satın Alma");
+            calisan2.CalisanBilgileri();
+
+            Calisan calisan3 = new Calisan("Zikriye", "Ürkmez");
+            calisan3.CalisanBilgileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public Calisan(string ad, string soyad, int no, string departman)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.No =   no;
+            this.Departman = departman;
+        }
+
+        public Calisan(string ad, string soyad)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+        }
+
+        public Calisan(){}
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışan Adı: {0}", Ad);
+            Console.WriteLine("Çalışan Soyadı: {0}", Soyad);
+            Console.WriteLine("Çalışan No: {0}", No);
+            Console.WriteLine("Çalışan Departmanı: {0}", Departman);
+        }
+    }
+}
