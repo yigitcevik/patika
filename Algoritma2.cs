0f8b5c8adb56f059486f89c076858c32c5bde672
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-09 22:58:32 +0300

    Add files via upload

---
+++ Algoritma2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kelime girin:");
+            string s = Console.ReadLine();
+            char a = s[0];
+            s = s.Remove(0,1);
+            Console.Write(s+a);
+        }
+    }
+}
