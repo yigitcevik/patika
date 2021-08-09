be162809dd28da872dd99edd5017fc515f368727
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-09 22:22:41 +0300

    Add files via upload

---
+++ Daire.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine());
+            int N = (2 * r + 1);
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    int x = i - r;
+                    int y = j - r;
+
+                    if (x * x + y * y <= r * r + 1) Console.Write("*");
+                    else Console.Write(" ");
+
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
