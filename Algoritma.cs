008ba5668c9fc8ba5a64b22ff916c601eb5a12c6
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-09 22:33:35 +0300

    Add files via upload

---
+++ Algoritma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(kelime,sayı) şeklinde değer giriniz:");
+            string[] s = Console.ReadLine().Split(",");
+            int a = Convert.ToInt32(s[1]);
+            Console.WriteLine(s[0].Remove(a,1));
+        }
+    }
+}
