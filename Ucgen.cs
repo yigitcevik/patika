99a7e7bfd14808bcf9b2ef081483b7dcece0efaa
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-09 21:31:18 +0300

    Add files via upload

---
+++ Ucgen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Boyut girin: ");  
+            int count = int.Parse(Console.ReadLine());  
+            for (int i = 0; i < count; i++) {  
+                for (int j = 0; j <= i; j++) {  
+                    Console.Write("*");  
+                }  
+                Console.Write("\n");  
+            }  
+        }
+    }
+}
