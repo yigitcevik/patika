81458b01efa1731a4a4394cf795630b58edeace3
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-10 00:00:34 +0300

    Add files via upload

---
+++ Hesaplamalar2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TEST
+{
+    class Program
+    {
+        static string[] a;
+        static void Main()
+        {
+            Console.WriteLine("(a,b) şeklinde 2 sayı giriniz: ");
+            a = Console.ReadLine().Split(",");
+            if (Convert.ToInt32(a[0]) == Convert.ToInt32(a[1]))
+            {
+                Console.WriteLine("Toplam Karesi: " + Math.Pow(Topla(),2));
+            }
+            else
+            {
+                Console.WriteLine("Toplam: " + Topla());
+            }
+            
+        }
+
+        public static int Topla()
+        {
+            int t = Convert.ToInt32(a[0]) + Convert.ToInt32(a[1]);
+            return t;
+        }
+    }
+}
