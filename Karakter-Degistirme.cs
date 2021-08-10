43daea7cb96bc2d224eeef0d8eb0641ae5ff5dcf
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-10 10:49:25 +0300

    Add files via upload

---
+++ Karakter-Degistirme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Kelime giriniz: ");
+            char[] s = Console.ReadLine().ToCharArray();
+            char f = s[0];
+            s[0] = s[s.Length-1];
+            s[s.Length-1] = f;
+            Console.WriteLine(s);
+        }        
+    }
+}
