6a6dab4d6ad342b6a4a25d6f5d13360692ec46ac
yigitcevik <abdullah.y.cevik@gmail.com>
2021-08-10 11:19:29 +0300

    Add files via upload

---
+++ Sessiz-Harf.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TEST
+{
+    class Program
+    {
+        static char[] sessiz = "bcçdfgğhjklmnprsştvyz".ToCharArray();
+        static void Main()
+        {
+            Console.WriteLine("Kelime giriniz: ");
+            char[] s = Console.ReadLine().ToCharArray();
+            int k = 0; //merhaba
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < sessiz.Length; j++)
+                {
+                    if (s[i] == sessiz[j])
+                    {
+                        for (int z = 0; z < sessiz.Length; z++)
+                        {
+                            if (i+1 < s.Length)
+                            {
+                                if (s[i + 1] == sessiz[z])
+                                {
+                                    k = 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (k == 1) Console.WriteLine(true);
+            else Console.WriteLine(false);
+        }
+    }
+}
